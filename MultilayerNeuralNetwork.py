7236433d7b5dc30c920f8cf1f87306922e1022e9
Abhijeet709 <40004232+Abhijeet709@users.noreply.github.com>
2020-11-04 00:14:09 +0530

    Update MultilayerNeuralNetwork.py

--- MultilayerNeuralNetwork.py
+++ MultilayerNeuralNetwork.py
@@ -27,30 +27,60 @@ class multiLayerNN:
         parameters = {}
         l = len(layerDims)
         for i in range(1,l):
-            parameters['W'+str(i)] = np.random.randn(layerDims[i],layerDims[i+1])*0.01
+            parameters['W'+str(i)] = np.random.randn(layerDims[i],layerDims[i-1])*0.01
             parameters['b'+str(i)] = np.zeros((layerDims[i],1))
-            return parameters
+        return parameters
 
-    def forwardActivation(self,parameters,X):
+    def linearforward(A, W, b):
+        Z = np.dot(W,A)+b
+        cache = (A, W, b)
+        return Z, cache
+    
+    def linearActivationForward(A_prev, W, b, activation):
+        if activation == "sigmoid":
+            Z, linear_cache = linearforward(A_prev, W, b)
+            A, activation_cache = sigmoid(Z)
+        elif activation == "relu":
+            Z, linear_cache = linearforward(A_prev, W, b)
+            A, activation_cache = relu(Z)
+        cache = (linear_cache, activation_cache)
+        return A, cache
+
+
+    def compute_cost(AL, Y):
+        m = Y.shape[1]
+        cost = np.sum(-((Y*np.log(AL))+((1-Y)*np.log(1-AL)))/m,axis = 1,keepdims = True)
+        cost = np.squeeze(cost)      # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
+        return cost
+    
+    def forward(self,parameters,X):
         caches = []
         l = len(parameters)//2
         A=X
         for i in range(1,l):
             A_prev = A
-            A,cache = self.relu(np.dot(parameters['W'+str(i)],A_prev)+ parameters['b'+str(i)])
+            A,cache = self.linearActivationForward(A_prev,parameters['W'+str(l)],parameters['b'+str(l)],"relu")
             caches.append(cache)
-        Al,cache = self.sigmoid(np.dot(parameters['W'+str(l)],A)+ parameters['b'+str(l)])
+        Al,cache = self.linearActivationForward(A_prev,parameters['W'+str(l)],parameters['b'+str(l)],"sigmoid")
         caches.append(cache)
         return Al,caches
 
-    def backwordLinearActivation(self,dA,cache,activation):
+    def linearbackward(dz,cache):
+        A_prev, W, b = cache
+        m = A_prev.shape[1]
+        dW = (np.dot(dZ,np.transpose(A_prev)))/m
+        db = np.sum(dZ,axis = 1,keepdims = True)/m
+        dA_prev = np.dot(np.transpose(W),dZ)
+        return dA_prev, dW, db
+    
+    def linearActivationBackward(self,dA,cache,activation):
         linear_cache,activation_cache = cache
         if activation == 'relu':
             dZ = self.relu_backward(dA,activation_cache)
-            dA_prev,dW,db = self.backward(dZ,linear_cache)
+            dA_prev,dW,db = self.linearbackward(dZ,linear_cache)
         else:
             dZ = self.sigmoid_backward(dA,activation_cache)
-            da_prev,dW,db = self.backward(dA,linear_cache)
+            da_prev,dW,db = self.linearbackward(dA,linear_cache)
         return dA_prev,dW,db
 
     def backward(self,AL,Y,caches):
